449c92ae59776caeafde3154af682a7f609f6654
Dev Sisodia <dsisodia@uoregon.edu>
2023-02-08 11:51:26 -0800

    Add files via upload

---
+++ hw2.py
@@ -0,0 +1,22 @@
+import data
+
+# Write your functions for each part in the space below.
+
+# Part 1
+
+
+# Part 2
+
+
+# Part 3
+
+
+# Part 4
+
+
+# Part 5
+
+
+# Part 6
+
+
---
+++ hw2_tests.py
@@ -0,0 +1,31 @@
+import data
+import hw2
+import unittest
+
+
+# Write your test cases for each part below.
+
+class TestCases(unittest.TestCase):
+    # Part 1
+
+
+    # Part 2
+
+
+    # Part 3
+
+
+    # Part 4
+
+
+    # Part 5
+
+
+    # Part 6
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
